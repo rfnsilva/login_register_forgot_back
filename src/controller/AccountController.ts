b378a29cb6fdd9e08b448e165ca82b7ce981cbf3
ricardotcu10 <ricardotcu10@gmail.com>
2020-07-27 02:43:45 -0300

    commit final final

--- src/controller/AccountController.ts
+++ src/controller/AccountController.ts
@@ -58,7 +58,7 @@ export const login = async (req: Request, res: Response) => {
     const { email, senha } = req.body;
 
     try {
-        const user = await getRepository(User).findOne({
+        const user = await getRepository(User).find({
             where: {
                 email
             }
@@ -71,9 +71,9 @@ export const login = async (req: Request, res: Response) => {
             });
 
             const data = {
-                id: user.id,
-                nome: user.nome,
-                email: user.email,
+                id: user[0].id,
+                nome: user[0].nome,
+                email: user[0].email,
                 token: token_login
             }
             
